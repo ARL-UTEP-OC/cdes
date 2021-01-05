6e8f454f121a41bf2631a9046c63edd5d274597f
raistlinJ <raistlinJ@users.noreply.github.com>
2021-01-05 10:12:56 -0700

    fixed sample bash script

--- sample/hooks/startcdes_runtime_hook.sh
+++ sample/hooks/startcdes_runtime_hook.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ###CHANGE THE FOLLOWING LINE TO THE DIRECTORY WITH YOUR CODE##
-CODE_PATH = /home/researchdev/cdes/
+CODE_PATH=/home/researchdev/cdes/
 
 
 ######DO NOT MODIFY (UNLESS YOU KNOW WHAT YOU"RE DOING)#######
