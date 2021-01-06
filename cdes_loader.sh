b604c0dc9334dba501952061cc4169fc84218d32
raistlinJ <raistlinJ@users.noreply.github.com>
2021-01-06 14:34:35 -0700

    Updated loader

--- cdes_loader.sh
+++ cdes_loader.sh
@@ -3,7 +3,7 @@
 #__license__ = "GPL 3.0"
 
 ###CHANGE THE FOLLOWING LINE TO THE DIRECTORY WITH YOUR CODE##
-CODE_PATH = /home/researchdev/cdes/
+CODE_PATH=/home/researchdev/cdes/
 
 
 ######DO NOT MODIFY (UNLESS YOU KNOW WHAT YOU"RE DOING)#######
